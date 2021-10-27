f2a3420d2fae530becbc043d531ce6d214faa019
Tiago <ntiagon@gmail.com>
2021-10-27 11:14:19 -0300

    testing

--- algo.js
+++ algo.js
@@ -1,1 +1,37 @@
-console.log("Hello World");
+const regioes = {
+  Norte: { entrada: false, saida: false },
+  Nordeste: { entrada: false, saida: false },
+  "Centro-Oeste": { entrada: false, saida: false },
+  Sudeste: { entrada: false, saida: false },
+  Sul: { entrada: false, saida: false },
+};
+
+const rotas = [
+  ["Norte", "Nordeste"],
+  ["Nordeste", "Sudeste"],
+  ["Sudeste", "Sul"],
+];
+
+function menorRota(routes) {
+  let result = 0;
+  for (const key in regioes) {
+    for (let i = 0; i < routes.length; i++) {
+      if (rotas[i][0] === key) {
+        regioes[key].entrada = true;
+      }
+      if (rotas[i][1] === key) {
+        regioes[key].saida = true;
+      }
+    }
+    if (regioes[key].entrada === true) {
+      result++;
+    }
+    if (regioes[key].saida === true) {
+      result++;
+    }
+  }
+  console.log(Math.ceil((10 - result) / 2));
+  return result;
+}
+
+menorRota(rotas);
